b362dce1949a47ff76eae022362f14a1342348f7
rakib <rakib135836@gmail.com>
2024-08-25 14:27:55 +0600

    jwt and verify token and hr

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
+// ---------------
+// basic
+// ----------------
+
+
+
+const express = require('express');
+// ----------------------------- 1
+const app = express();
+// -----------------------------2
+const cors = require('cors');
+// ---------------------------3
+
+require('dotenv').config();
+// -------------------------8
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000
+// -------------------------------------4
 
 // middleware
 const corsOptions = {
@@ -14,27 +26,15 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 }
+// ---------------------
 app.use(cors(corsOptions))
-
 app.use(express.json())
-app.use(cookieParser())
+// ------------------------5
+
+
 
 // Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token)
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
-    }
-    req.user = decoded
-    next()
-  })
-}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtepxet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const client = new MongoClient(uri, {
@@ -47,111 +47,151 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const assetCollection = client.db('assetDB').collection('asset')
-    const usersCollection = client.db('assetDB').collection('user')
+
+
+      // jwt related api
+      app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
+      })
+
+      // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+    // use verify admin after verifyToken
+    const verifyHr = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'hr';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+    // --------------------- older code -------------------------------------
+    // const assetCollection = client.db('assetDB').collection('asset')
+    // const usersCollection = client.db('assetDB').collection('user')
 
     // save a user data in db
-    app.put('/user', async (req, res) => {
-      const user = req.body
-      const query = { email: user?.email }
-      const options = { upsert: true }
-      const updateDoc = {
-        $set: {
-          ...user,
-          timestamp: Date.now(),
-        },
-      }
-      const result = await usersCollection.updateOne(query, updateDoc, options)
-      res.send(result)
-    })
+    // app.put('/user', async (req, res) => {
+    //   const user = req.body
+    //   const query = { email: user?.email }
+    //   const options = { upsert: true }
+    //   const updateDoc = {
+    //     $set: {
+    //       ...user,
+    //       timestamp: Date.now(),
+    //     },
+    //   }
+    //   const result = await usersCollection.updateOne(query, updateDoc, options)
+    //   res.send(result)
+    // })
 
     // Save an asset data in db
-    app.post('/asset', async (req, res) => {
-      const assetData = req.body
-      const result = await assetCollection.insertOne(assetData)
-      res.send(result)
-    })
+    // app.post('/asset', async (req, res) => {
+    //   const assetData = req.body
+    //   const result = await assetCollection.insertOne(assetData)
+    //   res.send(result)
+    // })
 
     // email query for asset list
-    app.get('/asset-lists/:email', async (req, res) => {
-      console.log(req.params.email)
-      console.log('tok tok token ', req.cookies)
-      let query = {}
-      if (req.query?.email) {
-        query = { email: req.query.email }
-      }
-      const result = await assetCollection.find(query).toArray()
-      res.send(result)
-    })
+    // app.get('/asset-lists/:email', async (req, res) => {
+    //   console.log(req.params.email)
+    //   console.log('tok tok token ', req.cookies)
+    //   let query = {}
+    //   if (req.query?.email) {
+    //     query = { email: req.query.email }
+    //   }
+    //   const result = await assetCollection.find(query).toArray()
+    //   res.send(result)
+    // })
 
     // delete an asset
-    app.delete('/asset/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await assetCollection.deleteOne(query)
-      res.send(result)
-    })
+    // app.delete('/asset/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await assetCollection.deleteOne(query)
+    //   res.send(result)
+    // })
 
     // getting assets for update
-    app.get('/getting-assets/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await assetCollection.findOne(query)
-      res.send(result)
-    })
+    // app.get('/getting-assets/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await assetCollection.findOne(query)
+    //   res.send(result)
+    // })
 
     // update asset
-    app.put('/assets/:id', async (req, res) => {
-      const id = req.params.id
-      const filter = { _id: new ObjectId(id) }
-      const update = {
-        $set: {
-          date: req.body.date,
-          product: req.body.product,
-          quantity: req.body.quantity,
-          type: req.body.type,
-        },
-      }
+    // app.put('/assets/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const filter = { _id: new ObjectId(id) }
+    //   const update = {
+    //     $set: {
+    //       date: req.body.date,
+    //       product: req.body.product,
+    //       quantity: req.body.quantity,
+    //       type: req.body.type,
+    //     },
+    //   }
 
-      try {
-        const result = await assetCollection.updateOne(filter, update)
-        res.send(result)
-      } catch (err) {
-        console.error(err)
-        res.status(500).send("Error updating booking.")
-      }
-    })
+    //   try {
+    //     const result = await assetCollection.updateOne(filter, update)
+    //     res.send(result)
+    //   } catch (err) {
+    //     console.error(err)
+    //     res.status(500).send("Error updating booking.")
+    //   }
+    // })
 
     // auth related api
-    app.post('/jwt', async (req, res) => {
-      const user = req.body
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-      })
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        })
-        .send({ success: true })
-    })
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: '365d',
+    //   })
+    //   res
+    //     .cookie('token', token, {
+    //       httpOnly: true,
+    //       secure: process.env.NODE_ENV === 'production',
+    //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //     })
+    //     .send({ success: true })
+    // })
 
     // Logout
-    app.get('/logout', async (req, res) => {
-      try {
-        res
-          .clearCookie('token', {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
-        console.log('Logout successful')
-      } catch (err) {
-        res.status(500).send(err)
-      }
-    })
+    // app.get('/logout', async (req, res) => {
+    //   try {
+    //     res
+    //       .clearCookie('token', {
+    //         maxAge: 0,
+    //         secure: process.env.NODE_ENV === 'production',
+    //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //       })
+    //       .send({ success: true })
+    //     console.log('Logout successful')
+    //   } catch (err) {
+    //     res.status(500).send(err)
+    //   }
+    // })
+
+
+    // --------------------- older code -------------------------------------
 
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
@@ -167,7 +207,9 @@ run().catch(console.dir)
 app.get('/', (req, res) => {
   res.send('Hello from opti-Asset Server..')
 })
+// ---------------------------------------6
 
 app.listen(port, () => {
   console.log(`opti-Asset is running on port ${port}`)
 })
+// -----------------------------------------------------7
