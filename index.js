d6b52488d0dc04332967ac1555387aa450474bb5
rakib <rakib135836@gmail.com>
2024-09-13 15:01:14 +0600

    finished

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 5000
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://opti-asset.web.app','http://localhost:5174','http://localhost:5173'],
   credentials: true,
   optionSuccessStatus: 200,
 }
