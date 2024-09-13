99f7f56cc8d3e7a3319a19677217cdae81801ffe
rakib <rakib135836@gmail.com>
2024-09-13 12:57:58 +0600

    all done

--- index.js
+++ index.js
@@ -628,28 +628,6 @@ async function run() {
 
     // 
 
-
-
-    // --------------------- older code -------------------------------------
-    // Logout
-    // app.get('/logout', async (req, res) => {
-    //   try {
-    //     res
-    //       .clearCookie('token', {
-    //         maxAge: 0,
-    //         secure: process.env.NODE_ENV === 'production',
-    //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    //       })
-    //       .send({ success: true })
-    //     console.log('Logout successful')
-    //   } catch (err) {
-    //     res.status(500).send(err)
-    //   }
-    // })
-
-
-    // --------------------- older code -------------------------------------
-
     // Send a ping to confirm a successful connection
     // await client.db('admin').command({ ping: 1 })
     console.log(
